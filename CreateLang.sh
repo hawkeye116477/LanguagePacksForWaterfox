7b84c904e0990852b42b9826492e7961ab1842a1
hawkeye116477 <hawkeye116477@gmail.com>
2019-09-21 22:06:07 +0200

    Add script for prepraring xpi

---
+++ CreateLang.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname "$0")
+
+cd "$SCRIPT_PATH" || exit
+
+# Build language XPI packages
+for D in *; do
+    if [ -d "${D}" ]; then
+        cd "${D}"
+        zip -r9 "$SCRIPT_PATH/../Waterfox/browser/extensions/${D%.*}.xpi" *
+        cd ..
+    fi
+done
